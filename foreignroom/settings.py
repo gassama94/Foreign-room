c2033187c87ad73264fdcee2eac35cbf21d81aa0
Saikou Gassama <gassamasaikou@yahoo.com>
2023-09-01 20:47:56 +0000

    Deployment commit, Added django url to allowed host

--- foreignroom/settings.py
+++ foreignroom/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['foreign-room.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['foreign-room-4472785d13b2.herokuapp.com' , 'foreign-room.herokuapp.com', 'localhost']
 
 
 # Application definition
