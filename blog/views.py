468d5f5fc55d67f220a552c64a7c8474129ede43
Saikou Gassama <gassamasaikou@yahoo.com>
2023-09-12 18:14:00 +0000

    Added unlike blog post method and buttons

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
@@ -12,11 +13,14 @@ from django.http import HttpResponseRedirect
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    liked = False
 
     if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
+            liked = False
     else:
             post.likes.add(request.user)
+            liked = True
 
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
        
@@ -32,6 +36,19 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, **kwargs):
+         context = super().get_context_data(**kwargs)
+         post = get_object_or_404(Post, id=self.kwargs['pk'])
+
+         liked = False
+         if self.request.user.is_authenticated and post.likes.filter(id=self.request.user.id).exists():
+             liked = True
+
+             context['liked'] = liked  # Add the 'liked' variable to the context
+         return context
+         
+
 
 
 class AddPostView(CreateView):
