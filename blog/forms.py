5a8d93fb11f05c97adf7f1b7d5132a2f2dbd7d66
Saikou Gassama <gassamasaikou@yahoo.com>
2023-09-16 18:46:58 +0000

    style profile page

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,11 @@
 from django import forms
-from .models import Post 
+from .models import Post
 from django.urls import reverse_lazy
-from allauth.account.forms import SignupForm 
+from allauth.account.forms import SignupForm
+from .views import UserChangeForm
+from django.contrib.auth.models import User
 from django.views import generic
+
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -18,15 +21,46 @@ widgets = {
     'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
     'author': forms.Select(attrs={'class': 'form-control'}),
     'content': forms.Textarea(attrs={'class': 'form-control'}),
-    
+
 }
 
 
 # forms.py
-class CustomSignupForm(SignupForm):
-    first_name = forms.CharField(
-        max_length=30, label='First Name', required=True)
-    last_name = forms.CharField(
-        max_length=30, label='Last Name', required=True)
-    
+class EditProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email',
+                  'last_login', 'is_superuser', 'is_active', 'date_joined']
 
+        email = forms.EmailField(widget=forms.EmailInput(
+            attrs={'class': 'form-control', 'style': 'width: 300px;'}))
+    first_name = forms.CharField(
+        max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(
+        max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(
+        max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_login = forms.DateTimeField(
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    is_superuser = forms.BooleanField(
+        widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    is_active = forms.BooleanField(
+        widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    date_joined = forms.DateTimeField(
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+
+#######
+class CustomSignupForm(SignupForm):
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'style': 'width: 300px;'}))
+    first_name = forms.CharField(
+        max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(
+        max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(
+        max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}))
--- blog/views.py
+++ blog/views.py
@@ -4,25 +4,20 @@ from django.contrib.auth.forms import UserChangeForm
 from django.db import models
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-#from .views import UserEditView
+# from .views import UserEditView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditProfileForm
 from django.urls import reverse_lazy, reverse
 from django.http import HttpResponseRedirect
 
 
-
 class UserEditView(UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = 'edit_profile.html'
     success_url = reverse_lazy('home')
 
-
     def get_object(self):
-         return self.request.user
-
-
-
+        return self.request.user
 
 
 def LikeView(request, pk):
@@ -30,15 +25,13 @@ def LikeView(request, pk):
     liked = False
 
     if post.likes.filter(id=request.user.id).exists():
-            post.likes.remove(request.user)
-            liked = False
+        post.likes.remove(request.user)
+        liked = False
     else:
-            post.likes.add(request.user)
-            liked = True
+        post.likes.add(request.user)
+        liked = True
 
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
-       
-
 
 
 class HomeView(ListView):
@@ -52,42 +45,38 @@ class ArticleDetailView(DetailView):
     template_name = 'article_details.html'
 
     def get_context_data(self, **kwargs):
-         context = super().get_context_data(**kwargs)
-         post = get_object_or_404(Post, id=self.kwargs['pk'])
+        context = super().get_context_data(**kwargs)
+        post = get_object_or_404(Post, id=self.kwargs['pk'])
 
-         liked = False
-         if self.request.user.is_authenticated and post.likes.filter(id=self.request.user.id).exists():
-             liked = True
+        liked = False
+        if self.request.user.is_authenticated and post.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
-             context['liked'] = liked  # Add the 'liked' variable to the context
-         return context
-         
-
+            context['liked'] = liked  # Add the 'liked' variable to the context
+        return context
 
 
 class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
-    # fields = ('title', 
+    # fields = ('title',
     # 'title_tag',
-    # 'author', 
+    # 'author',
     # 'content',
-    # 'featured_image', 
+    # 'featured_image',
     # 'status')
     # fields = '__all__'
     # fields = ('title', 'content')
+
 
 class UpdatePostView(UpdateView):
     model = Post
     template_name = 'update_post.html'
     fields = ['title', 'title_tag', 'content']
-   
 
 
 class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
-   
-   
